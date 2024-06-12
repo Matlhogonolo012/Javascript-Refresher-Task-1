a2f096ec5a96780c5056a203972d3ec763eacc97
Matlhogonolo012 <tlhoxi12@gmail.com>
2024-06-12 21:40:14 +0200

    ALL

--- Variables.js
+++ Variables.js
@@ -1,6 +1,45 @@
+// Exercise 1: Variable Declaration
+// 1.
 var name = "Matlhogonolo";
+
+// 2.
 var age = "24";
+
+// 3.
 var isStudent = true;
 
-console.log("name:", Matlhogonolo)
-console.log("age")
+// 4.
+console.log("My Name:", name)
+console.log("My Age", age)
+console.log("Am I A student", isStudent)
+
+// Exercise 2: Variable Reassignment
+// 1.
+var favoriteColour = "Purple";
+
+// 2.
+console.log("My favourite colour is", favoriteColour)
+
+// 3.
+favoriteColour = "Blue"
+
+// 4.
+console.log = ("My new favourite colour is", favoriteColour);
+
+//Exercise 3: Arithmetic Operations
+// 1.
+var num1 = 15;
+var num2 = 10;
+
+// 2.
+var sum, difference, product, quotien;
+sum = num1 + num2;
+difference = num1 - num2;
+product = num1 * num2;
+quotien = num1 / num2;
+
+// 3.
+console.log("sum:", sum)
+console.log("difference:", difference)
+console.log("product:", product)
+console.log("quotien:", quotien)
